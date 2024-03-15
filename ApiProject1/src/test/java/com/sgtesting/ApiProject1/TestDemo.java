6babbdd6a001b6620fed31dcc5f19d117e1d0ee2
DivyaSubramanyam123 <you@example.com>
2024-03-15 16:45:14 +0530

    Firstcommit

---
+++ ApiProject1/src/test/java/com/sgtesting/ApiProject1/TestDemo.java
@@ -0,0 +1,16 @@
+package com.sgtesting.ApiProject1;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+@Test
+void TestDemo1()
+{
+	System.out.println("Test1");
+}
+@Test
+void TestDemo2()
+{
+	System.out.println("Test2");
+}
+}
